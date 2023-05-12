14dd6cfca0206aa35826052f96b6e6ce1954e27e
Gabriel <45741342+takegabriel08@users.noreply.github.com>
2023-05-12 17:37:53 +0300

    Update create_product.php
    added category array similar to product one

--- create_product.php
+++ create_product.php
@@ -5,7 +5,7 @@ class ControllerCreateProduct extends Controller
     {
         $this->load->model('catalog/product');
 
-        $url = 'https://www.websiteExample.ro/feed-sxml.php';
+        $url = 'https://www.natur.ro/feed-sxml.php';
         $xml = simplexml_load_file($url);
 
         $limit = 20;
@@ -17,10 +17,43 @@ class ControllerCreateProduct extends Controller
             }
 
             $imageUrl = $result->image;
-            $imagePath = 'catalog/demo/custom/' . trim($result->title) . '.png';
+            $imagePath = 'catalog/demo/custom/' . trim($result->title) . '.' . pathinfo($imageUrl, PATHINFO_EXTENSION);
 
             $imageContent = file_get_contents($imageUrl);
             file_put_contents(DIR_IMAGE . $imagePath, $imageContent);
+
+            $category = array(
+                'category_description' => array(
+                    // You should replace 1 with the actual language_id for the language you want to use.
+                    1 => array(
+                        'name' => 'demo',
+                        'description' => '',
+                        'meta_title' => 'demo',
+                        'meta_description' => '',
+                        'meta_keyword' => '',
+                        'tag' => ''
+                    )
+                ),
+                'path' => '',
+                'parent_id' => '0',
+                'filter' => '',
+                'category_store' => array(
+                    0 => '0'
+                ),
+                'image' => '',
+                'top' => '1',
+                'column' => '2',
+                'sort_order' => '0',
+                'status' => '1',
+                'category_seo_url' => array(
+                    0 => array(
+                        1 => ''
+                    )
+                ),
+                'category_layout' => array(
+                    0 => ''
+                )
+            );
 
             $data = array(
                 'product_description' => array(
