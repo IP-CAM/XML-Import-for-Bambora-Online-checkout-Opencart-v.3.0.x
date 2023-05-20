a6726e1d7b241c51e99768ea59ce4e1e5b037c48
Gabriel <45741342+takegabriel08@users.noreply.github.com>
2023-05-20 23:39:03 +0300

    Update image_optimizer.php
    added additional checks, if the image is smaller than a giver point it is no longer optimized for time saving and resources...

--- image_optimizer.php
+++ image_optimizer.php
@@ -1,42 +1,49 @@
 <?php
-function optimize_image($imageUrl, $imagePath)
+function optimize_image($imageUrl, $imagePath, $maxFileSizeKB = 300)
 {
     $imageContent = file_get_contents($imageUrl);
-    $sourceImage = imagecreatefromstring($imageContent);
+    $fileSizeKB = strlen($imageContent) / 1024;
 
-    $width = imagesx($sourceImage);
-    $height = imagesy($sourceImage);
+    if ($fileSizeKB > $maxFileSizeKB) {
+        $sourceImage = imagecreatefromstring($imageContent);
 
-    $maxSize = 600; // Max width or height
-    if ($width > $maxSize || $height > $maxSize) {
-        // Calculate new size
-        if ($width > $height) {
-            $newWidth = $maxSize;
-            $newHeight = round($height * ($maxSize / $width));
+        $width = imagesx($sourceImage);
+        $height = imagesy($sourceImage);
+
+        $maxSize = 600; // Max width or height
+        if ($width > $maxSize || $height > $maxSize) {
+            // Calculate new size
+            if ($width > $height) {
+                $newWidth = $maxSize;
+                $newHeight = round($height * ($maxSize / $width));
+            } else {
+                $newHeight = $maxSize;
+                $newWidth = round($width * ($maxSize / $height));
+            }
+
+            // Create a new, resized image
+            $newImage = imagecreatetruecolor($newWidth, $newHeight);
+            imagecopyresampled($newImage, $sourceImage, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+            // Save the new image
+            switch (pathinfo($imageUrl, PATHINFO_EXTENSION)) {
+                case 'jpeg':
+                case 'jpg':
+                    imagejpeg($newImage, DIR_IMAGE . $imagePath, 77); // 85 is quality
+                    break;
+                case 'png':
+                    imagepng($newImage, DIR_IMAGE . $imagePath, 8); // 8 is compression level
+                    break;
+                case 'gif':
+                    imagegif($newImage, DIR_IMAGE . $imagePath);
+                    break;
+            }
         } else {
-            $newHeight = $maxSize;
-            $newWidth = round($width * ($maxSize / $height));
-        }
-
-        // Create a new, resized image
-        $newImage = imagecreatetruecolor($newWidth, $newHeight);
-        imagecopyresampled($newImage, $sourceImage, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
-
-        // Save the new image
-        switch (pathinfo($imageUrl, PATHINFO_EXTENSION)) {
-            case 'jpeg':
-            case 'jpg':
-                imagejpeg($newImage, DIR_IMAGE . $imagePath, 77); // 85 is quality
-                break;
-            case 'png':
-                imagepng($newImage, DIR_IMAGE . $imagePath, 8); // 8 is compression level
-                break;
-            case 'gif':
-                imagegif($newImage, DIR_IMAGE . $imagePath);
-                break;
+            // If image is smaller than max size, save it as it is
+            file_put_contents(DIR_IMAGE . $imagePath, $imageContent);
         }
     } else {
-        // If image is smaller than max size, save it as it is
+        // If image is smaller than max size in MB, save it as it is
         file_put_contents(DIR_IMAGE . $imagePath, $imageContent);
     }
 }
